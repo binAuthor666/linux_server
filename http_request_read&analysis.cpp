3cd660f5862d13bde63263450bf1e2c717369f7e
binArthur <bin_author666@163.com>
2023-05-25 19:57:02 +0800

    add explanatory to file 有限状态机

--- http_request_read&analysis.cpp
+++ http_request_read&analysis.cpp
@@ -8,37 +8,49 @@
 #include<errno.h>
 #include<string.h>
 #include<fcntl.h>
-#define BUFFER_SIZE 4096
-
+#define BUFFER_SIZE 4096 /*缓冲区大小*/
+/*主状态机的两种可能状态，分别表示：当前正在分析请求行，当前正在分析头部字段 */
 enum CHECK_STATE{CHECK_STATE_REQUESTLINE=0,CHECK_STATE_HEADER};
-
+/*从状态机的三种可能状态，即行的读取状态：读取到一个完整的行、行出错、行数据尚且不完整*/
 enum LINE_STATUS{LINE_OK=0,LINE_BAD,LINE_OPEN};
-
+/*服务器处理HTTP请求的结果：NO_REQUEST表示请求不完整，需要继续读取客户数据；GET_REQUEST表示
+获得了一个完整的客户请求；BAD_REQUEST表示请求有语法错误；FORBIDDEN_REQUEST表示客户对资源没有
+足够的访问权限；INTERNAL_REQUEST表示服务器内部错误；CLOSED_CONNECTION表示客户端已经关闭连接*/
 enum HTTP_CODE{NO_REQUEST,GET_REQUEST,BAD_REQUEST,
 FORBIDDEN_REQUEST,INTERNAL_ERROR,CLOSED_CONNECTION};
-
+/*给服务器返回的处理结果*/
 static const char* szret[]={"I get a correct result\n","Something wrong\n"};
-
+/*从状态机，用于解析出一行内容*/
+//checked_index:指向buffer中当前正在分析的字节
+//read_index:指向buffer中客户数据的尾部的下一字节
 LINE_STATUS parse_line(char* buffer,int& checked_index,int& read_index)
 {
     char temp;
+    
     for(;checked_index<read_index;++checked_index)
     {
+        /*获取当前要分析的字节*/
         temp=buffer[checked_index];
+        /*如果当前的字节是回车符，即说明可能读取到一个完整行*/
         if(temp=='\r')
         {
+            /*如果'\r'字符碰巧是当前最好一个被读入的客户数据，那么这次分析没有读取到完整
+            的一行，返回LINE_OPEN以表示需要继续读取*/
             if((checked_index+1)==read_index)
             {
                 return LINE_OPEN;
             }
+            /*如果下一个字符是'\n'，说明读取到了完整的行*/
             else if(buffer[checked_index+1]=='\n')
             {
                 buffer[checked_index++]='\0';
                 buffer[checked_index++]='\0';
                 return LINE_OK;
             }
+            /*否则的话，说明客户发送的http请求存在语法问题*/
             return LINE_BAD;
         }
+        /*如果当前的字节是换行符，即说明可能读取到一个完整行*/
         else if(temp=='\n')
         {
             if((checked_index>1)&&buffer[checked_index-1]=='\r')
@@ -50,13 +62,15 @@ LINE_STATUS parse_line(char* buffer,int& checked_index,int& read_index)
             return LINE_BAD;
         }
     }
-    return LINE_BAD;
+    /*如果所有内容都分析完毕也没遇到'\r'字符，返回LINE_OPEN,表示还需要继续读取客户数据*/
+    return LINE_OPEN;
     
 }
-
+/*分析请求行*/
 HTTP_CODE parse_requestline(char* temp,CHECK_STATE& checkstate)
 {
     char* url=strpbrk(temp," \t");
+    /*如果请求行中没有空白字符或'\t'字符，则HTTP请求必有问题*/
     if(!url)
     {
         return BAD_REQUEST;
@@ -95,42 +109,44 @@ HTTP_CODE parse_requestline(char* temp,CHECK_STATE& checkstate)
         return BAD_REQUEST;
     }
     printf("The request URL is:%s\n",url);
+    /*HTTP请求行处理完毕，状态转移到头部字段的分析*/
     checkstate=CHECK_STATE_HEADER;
     return NO_REQUEST;
 }
-
+/*分析头部字段*/
 HTTP_CODE parse_headers(char* temp)
 {
+    /*遇到一个空行，说明我们得到了一个正确的HTTP请求*/
     if(temp[0]=='\0')
     {
         return GET_REQUEST;
     }
-    else if(strncasecmp(temp,"Host:",5)==0)
+    else if(strncasecmp(temp,"Host:",5)==0)/*处理HOST头部字段*/
     {
         temp+=5;
         temp+=strspn(temp," \t");
         printf("the request host is:%s\n",temp);
     }
-    else
+    else/*其他头部字段都不处理*/
     {
         printf("I can not handle this header\n");
     }
     return NO_REQUEST;
 }
-
+/*分析HTTP请求的入口函数*/
 HTTP_CODE parse_content(char* buffer,int& checked_index,CHECK_STATE&
                         checkstate,int& read_index,int& start_line)
 {
-    LINE_STATUS linestatus=LINE_OK;
-    HTTP_CODE retcode=NO_REQUEST;
-
+    LINE_STATUS linestatus=LINE_OK;/*记录当前行的读取状态*/
+    HTTP_CODE retcode=NO_REQUEST;/*记录HTTP请求的处理结果*/
+    /*主状态机，用于从buffer中取出所有完整的行*/
     while((linestatus=parse_line(buffer,checked_index,read_index))==LINE_OK)
     {
-        char* temp=buffer+start_line;
-        start_line=checked_index;
+        char* temp=buffer+start_line; /*start_line是行在buffer中的起始位置*/
+        start_line=checked_index;/*记录下一行的起始位置*/
         switch(checkstate)
         {
-            case CHECK_STATE_REQUESTLINE:
+            case CHECK_STATE_REQUESTLINE:/*第一个状态，分析请求行*/
             {
                 retcode=parse_requestline(temp,checkstate);
                 if(retcode==BAD_REQUEST)
@@ -139,7 +155,7 @@ HTTP_CODE parse_content(char* buffer,int& checked_index,CHECK_STATE&
                 }
                 break;
             }
-            case CHECK_STATE_HEADER:
+            case CHECK_STATE_HEADER:/*第二个状态，分析消息头*/
             {
                 retcode=parse_headers(temp);
                 if(retcode==BAD_REQUEST)
